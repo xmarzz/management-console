c30750f825664e861779421367b5b9e8450a5473
Marz <sathickjaffer44@gmail.com>
2023-02-19 20:30:07 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Sidebar from '../Components/Sidebar'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component,pageProps}) {
+  return (
+  <Sidebar>
+  <Component {...pageProps} />
+  </Sidebar>
+  );
 }
